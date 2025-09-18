fc466996b840bcd2179c931c6b5850eec6afd5e9
RahJade21 <jaydregon@gmail.com>
2025-09-18 14:48:34 +0700

    add feature for video card display, fetch from api

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,7 @@ import { Stack } from '@mui/material';
 
 import { categories } from '../utils/constants';
 
-const selectedCategory = 'New';
-
-const Sidebar = () => (
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
   <Stack
     direction="row"
     sx={{
@@ -14,7 +12,11 @@ const Sidebar = () => (
     }}
   >
     {categories.map((category) => (
-      <button className='category-btn' key={category.name} style={{ background: category.name === selectedCategory && '#FC1503', color: 'white' }}>
+      <button 
+        className='category-btn'
+        onClick={() => setSelectedCategory(category.name)}
+        key={category.name} 
+        style={{ background: category.name === selectedCategory && '#FC1503', color: 'white' }}>
         <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px' }}>{category.icon}</span>
         <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>{category.name}</span>
       </button>
