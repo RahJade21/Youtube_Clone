859ea023bc609977edb7b67675dec2563f9a37db
RahJade21 <jaydregon@gmail.com>
2025-09-24 15:38:45 +0700

    finished projet with adjustmen for youtube videos

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChanneldCard, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
+  const [channelDetail, setchannelDetail] = useState(null)
+  const [videos, setVideos] = useState([]);
+
+  const { id } = useParams();
+  
+  console.log(channelDetail, videos);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part="snippet&id=${id}`)
+      .then((data) => setchannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos(data?.items));
+  }, [id])
+
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh">
+      <Box>
+      <div style={{ 
+        background: 'linear-gradient(90deg,rgba(131, 58, 180, 1) 0%, rgba(253, 29, 29, 1) 50%, rgba(252, 176, 69, 1) 100%)', 
+        zIndex: 10, 
+        height: '300px' 
+      }} 
+      />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box p={2} display="flex">
+        <Box sx={{ mr: { sm: '100px' }}} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
   )
 }
 
